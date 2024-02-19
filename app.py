49fe428d521343325a7c6a15cfdaf3f6bde91724
abahr35 <austinbahr97@gmail.com>
2024-02-19 16:00:18 -0600

    updated the site with the basics of OOP in HTML with Jinja2

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Get2Helping'
+    return render_template('home.html', name="Get2Helping")
 
 
 if __name__ == '__main__':
