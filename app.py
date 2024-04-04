134dbb35667c0e3cbb80aa366e88751ec9f7162a
Normando13 <neiljnormando@lewisu.edu>
2024-04-03 21:16:25 -0500

    adding about page

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import dotenv
+import os
+from flask import Flask, render_template, flash
 from googleplaces import GooglePlaces
+
 from forms import LocationInputForm
-import dotenv, os
+
 dotenv.load_dotenv()
 gtoken = str(os.getenv("GTOKEN"))
 flasktoken = str(os.getenv("FLASKTOKEN"))
@@ -14,9 +17,9 @@ gglObj = GooglePlaces(gtoken)
 def home():  # put application's code here
     return render_template('home.html', name="Get2Helping")
 
-@app.route('/Mission')
+@app.route('/about')
 def about():  # put application's code here
-    return render_template('Mission.html', name="Mission")
+    return render_template('about.html', name="About")
 
 @app.route('/testmaps')
 def test_maps():
